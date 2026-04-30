2b81eba57b908a115743b4f2aaaaf1a1b47153aa
anayolico <acnwa1234@gmail.com>
2026-04-30 20:31:04 +0100

    add

--- src/server.js
+++ src/server.js
@@ -11,11 +11,11 @@ const hasResendApiKey =
   resendApiKey && !resendApiKey.includes('your_api_key_here');
 const resend = hasResendApiKey ? new Resend(resendApiKey) : null;
 
-const defaultClientOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5174',
-  'https://my-portfolio-beryl-two-rfjdabbgrm.vercel.app',
-];
+// const defaultClientOrigins = [
+//   'http://localhost:5173',
+//   'http://localhost:5174',
+//   'https://my-portfolio-beryl-two-rfjdabbgrm.vercel.app',
+// ];
 const allowedOrigins = (process.env.CLIENT_ORIGIN || defaultClientOrigins.join(','))
   .split(',')
   .map((origin) => origin.trim())
